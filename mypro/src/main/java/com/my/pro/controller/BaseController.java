c3342083978b17f44fbe2aff05737087610e7f67
Rupesh Sharma <rupesh.sh@HCL.COM>
2016-01-18 14:55:54 +0530

    Added spring configuration and base controller

---
+++ mypro/src/main/java/com/my/pro/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.my.pro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BaseController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String toHomePage(Model model) {
+		model.addAttribute("msg", "THIS IS SAMPLE");
+		return "home";
+	}
+
+}
---
+++ mypro/src/main/webapp/WEB-INF/views/jsp/home.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>MY PRO SAMPLE</title>
+</head>
+<body>
+	<h4>MY PRO SAMPLE</h4>
+	says:
+	<span class="blue">${msg}</span>
+</body>
+</html>
